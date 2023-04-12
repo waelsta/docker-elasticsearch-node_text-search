30a63b747ef12436af9482652aeb77ace213edc7
waelsta <waelsta055@gmail.com>
2023-04-12 14:14:40 +0100

    added autocomplete and fuzzy search

--- controller/indexingController.js
+++ controller/indexingController.js
@@ -1,47 +1,73 @@
-const {Client} = require('@elastic/elasticsearch');
+const { Client } = require("@elastic/elasticsearch");
 
 //create an instance of elastic client
 const client = new Client({
-    node: 'http://localhost:9200',
-    log: 'trace'
+  node: "http://localhost:9200",
+  log: "trace",
 });
 
-
 //declare the index function
-const indexDocument = async function(index, body){
-    const {body: response} = await client.index({
-        index: index,
-        body: body
-    });
+const indexDocument = async function (index, body) {
+  const { body: response } = await client.index({
+    index: index,
+    body: body,
+  });
 
-    return response;
-}
-
+  return response;
+};
 
 //below code creates a new index using indexDocument function with the index name and body
-indexDocument('my_index', {
-    title: 'Full text search with ElasticSearch on docker',
-    content:'here is the content of the text.....'
-}).then(response => console.log(response))
-.catch(err => console.log(err));
-
+indexDocument("my_index", {
+  title: "Full text search with ElasticSearch on docker",
+  content: "here is the content of the text.....",
+})
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
 
 //declare the search function
-const searchDocuments = async function(query) {
-    const {body} = await client.search({
-        index: 'my_index',
+const searchDocuments = async function (query) {
+  const { body } = await client.search({
+    index: "my_index",
+    body: {
+      query: {
+        fuzzy: {
+          content: {
+            value: query,
+            fuzziness: 'AUTO'
+          },
+        },
+      },
+    },
+  });
+
+  return body.hits.hits;
+};
+
+//implementing autocomplete search with completion suggester
+const autocompleteSearch = async function (query) {
+    const { body } = await client.search({
+        index: "my_index",
         body: {
-            query: {
-                match: {
-                    content: query
+            suggest: {
+                suggest_title: {
+                    prefix: query,
+                    completion: {
+                        field: "title_suggest",
+                        //could add fuzzy here
+                    }
                 }
             }
         }
     });
 
-    return body.hits.hits;
-}
+    return body.suggest.suggest_title[0].options.map(option => option.text);
+};
 
 //below code searches for the text in the index
-searchDocuments('ElasticSearch on Docker').then(response => console.log(response))
-.catch(error => console.log(error));
+searchDocuments("ElasticSearch on Dockr")
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
+
+  autocompleteSearch('ElasticSearch')
+  .then(results => console.log(results))
+  .catch(error => console.error(error));
